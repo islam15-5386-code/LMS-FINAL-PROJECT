231829f85c9e907c77a2841892fce2f61cb2e760
Remon-771 <remon2305101584@diu.edu.bd>
2026-05-04 21:44:10 -0700

    push

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -4,3 +4,4 @@ import "./.next/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+// If you need to add custom types, you can do so in a separate file, such as `types.d.ts`, and import it here.
